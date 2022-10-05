c86193f7b7fd762489df6030c4d37d02472ee40c
Kevin Taufer <julpod132@gmail.com>
2022-10-05 14:08:19 +0200

    check if user is a bot -> don't count

--- listeners/events/index.js
+++ listeners/events/index.js
@@ -33,6 +33,28 @@ const registerListenerForTeamEmojis = async (app) => {
             }
           }
 
+          // check if there is a mention to in the message
+          const matched = userMentionedRegex.exec(message.text);
+          let mention_to_id = undefined;
+          if (matched && matched.length > 0) {
+            const user_id = matched[1];
+            if (user_id !== message.user) {
+              mention_to_id = user_id;
+            }
+          }
+
+          // check if user is bot user
+          const {
+            data: { user },
+          } = await axios.post(
+            "https://slack.com/api/users.info",
+            new URLSearchParams({ user: message.user }),
+            { headers: { Authorization: `Bearer ${team.bot.token}` } }
+          );
+
+          // if user is a bot cancel
+          if (user.is_bot) return;
+
           countEmoji(
             message.team,
             message.user,
